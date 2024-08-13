ae8f90c4c194db9dc954efeb09b4a0e874db45d2
Guaracy A. Lima <guaracyaraujolima@gmail.com>
2024-08-13 11:24:07 -0300

    ïnitial commit

---
+++ src/main/java/br/com/_th/glam_config_srv/GlamConfigSrvApplication.java
@@ -0,0 +1,13 @@
+package br.com._th.glam_config_srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlamConfigSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlamConfigSrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/_th/glam_config_srv/GlamConfigSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com._th.glam_config_srv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlamConfigSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
